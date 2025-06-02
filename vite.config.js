c78d36a31c291534fe7447a3ea0f4186038e2320
Satya Prakash Pandey <satya.pandey@qualitestgroup.com>
2025-06-02 14:38:01 +0530

    remove checker

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [
     react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx}"', // Adjust if your code is elsewhere
-      },
-    }),
   ],
 });
