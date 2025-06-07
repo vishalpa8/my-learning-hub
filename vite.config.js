17e7219b59fec051fb8f98010d44e17605a6da0f
Satya Prakash Pandey <satya.pandey@qualitestgroup.com>
2025-06-07 10:19:49 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://my-learning-hub-2a2c80.gitlab.io/",
+  base: "/",
   plugins: [react()],
 });
