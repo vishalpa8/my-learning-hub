72b29f28682937aad377b9bafc29e7989bc7d8c4
Satya Prakash Pandey <satya.pandey@qualitestgroup.com>
2025-06-08 22:41:43 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Make sure your global styles are imported here
+import { BrowserRouter } from 'react-router-dom' // 1. Import BrowserRouter
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap App and add the basename */}
+    <BrowserRouter basename="/my-learning-hub/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
