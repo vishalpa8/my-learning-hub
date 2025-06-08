e3265521f0819e6793f94891c6f6ef610aa6f074
Satya Prakash Pandey <satya.pandey@qualitestgroup.com>
2025-06-08 22:27:15 +0530

    added conf

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Set the base path for the application
+  base: "/my-learning-hub", // Set the base path for the application
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true,
-  },
 });
